01ccf6d2c248abf42de3ca4f1cb8ac1ae52d21ed
Aleksandr Alekhin <alekhin.aleks92@gmail.com>
2024-12-05 14:56:34 +0100

    fix bnner

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,21 +1,22 @@
-import {Button} from '../../ui/Button';
-import styles from './Banner.module.css';
 import React from "react";
+import { Button } from '../../ui/Button';
+import styles from './Banner.module.css';
+// @ts-expect-error // eslint-line
+import bannerImg from '../../../public/assets/img/bannerImg.png';
 
 export const Banner = () => {
     return (
-    <>
-    <section className="first-screen">
-      <div className="rectangle-2"></div>
-      <div
-        className={styles.rectangle3}
-      ></div>
-      <div className="banner-content">
-        <h1>DISCOVER YOUR INNER BEAUTY<br />WITH BLOSSOM GLOW KIT</h1>
-        <p>Great gift for yourself and loved ones</p>
-        <Button className="banner-button" onClick={()=>{alert('Working')}}>Shop Now</Button>
-      </div>
-    </section>
-    </>
-  );
-}
+        <section className={styles.firstScreen}>
+             <div className={styles.rectangle2}></div>
+            <div
+                className={styles.rectangle3}
+                style={{ backgroundImage: `url(${bannerImg})` }}
+            ></div>
+            <div className={styles.bannerContent}>
+                <h1>DISCOVER YOUR INNER BEAUTY<br />WITH BLOSSOM GLOW KIT</h1>
+                <p>Great gift for yourself and loved ones</p>
+                <Button className={styles.bannerButton} onClick={() => { alert('Working') }}>Shop Now</Button>
+            </div>
+        </section>
+    );
+};
--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import  './footer.css';
+import footerStyles from './Footer.module.css';
 // @ts-expect-error" // eslint-line
 import facebook from '../../../public/assets/img/facebook.png'
 // @ts-expect-error" // eslint-line
@@ -14,74 +14,96 @@ import telegram from '../../../public/assets/img/telegram.png'
 
 function Footer() {
   return (
-    <footer className="footer-container">
-        <div className="footer-section social-icons">
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <img src={facebook} alt="Facebook" />
-          </a>
-          <a href="https://pinterest.com" target="_blank" rel="noopener noreferrer">
-            <img src={pinterst} alt="Pinterest" />
-          </a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <img src={twitter} alt="Twitter" />
-          </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <img src={instagram} alt="Instagram" />
-          </a>
-          <a href="https://telegram.org" target="_blank" rel="noopener noreferrer">
-            <img src={telegram} alt="Telegram" />
-          </a>
-        </div>
-      <div className="footer-main">
-        <div className="footer-section company-info">
-          <h3><span className="brand-name">Bloom</span> Beauty</h3>
+    <footer className={footerStyles.footerContainer}>
+      <div className={`${footerStyles.footerSection} ${footerStyles.socialIcons}`}>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <img src={facebook} alt="Facebook" />
+        </a>
+        <a href="https://pinterest.com" target="_blank" rel="noopener noreferrer">
+          <img src={pinterst} alt="Pinterest" />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <img src={twitter} alt="Twitter" />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src={instagram} alt="Instagram" />
+        </a>
+        <a href="https://telegram.org" target="_blank" rel="noopener noreferrer">
+          <img src={telegram} alt="Telegram" />
+        </a>
+      </div>
+      <div className={footerStyles.footerMain}>
+        <div className={`${footerStyles.footerSection} ${footerStyles.companyInfo}`}>
+          <h3>
+            <span className={footerStyles.brandName}>Bloom</span> Beauty
+          </h3>
           <p>Discover nature&#39;s beauty with our natural care products.</p>
           <p>
-            <span>📞 +38 050 123 45 67</span><br />
-            <span>📧 bloombt@gmail.com</span><br />
+            <span>📞 +38 050 123 45 67</span>
+            <br />
+            <span>📧 bloombt@gmail.com</span>
+            <br />
             <span>📍 Kyiv, Ukraine</span>
           </p>
         </div>
-        <div className="footer-section">
+        <div className={footerStyles.footerSection}>
           <h3>Help</h3>
           <ul>
-            <li><a href="#">Contact us</a></li>
-            <li><a href="#">FAQ</a></li>
-            <li><a href="#">Shipping & Returns</a></li>
+            <li>
+              <a href="#">Contact us</a>
+            </li>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+            <li>
+              <a href="#">Shipping & Returns</a>
+            </li>
           </ul>
         </div>
 
-        <div className="footer-section">
+        <div className={footerStyles.footerSection}>
           <h3>My Account</h3>
           <ul>
-            <li><a href="#">Addresses</a></li>
-            <li><a href="#">Order Status</a></li>
-            <li><a href="#">Wishlist</a></li>
+            <li>
+              <a href="#">Addresses</a>
+            </li>
+            <li>
+              <a href="#">Order Status</a>
+            </li>
+            <li>
+              <a href="#">Wishlist</a>
+            </li>
           </ul>
         </div>
 
-        <div className="footer-section third">
+        <div className={`${footerStyles.footerSection} ${footerStyles.third}`}>
           <h3>Customer Care</h3>
           <ul>
-            <li><a href="#">About us</a></li>
+            <li>
+              <a href="#">About us</a>
+            </li>
           </ul>
         </div>
 
-        <div className="footer-section footer-subscribe">
+        <div className={`${footerStyles.footerSection} ${footerStyles.footerSubscribe}`}>
           <h3>Sign Up for Emails</h3>
           <p>Stay informed, subscribe to our newsletter now!</p>
-          <div className="subscribe-form">
+          <div className={footerStyles.subscribeForm}>
             <input type="email" placeholder="Email" />
-            <a className="subscribe-form-link">Subscribe →</a>
+            <a className={footerStyles.subscribeFormLink}>Subscribe →</a>
           </div>
         </div>
       </div>
 
-      <div className="footer-bottom">
+      <div className={footerStyles.footerBottom}>
         <p>&copy; 2023 Bloom Beauty</p>
-        <div className="footer-links">
-          <a className="footer-link" href="#">Privacy Policy</a>
-          <a className="footer-link" href="#">Terms and Conditions</a>
+        <div className={footerStyles.footerLinks}>
+          <a className={footerStyles.footerLink} href="#">
+            Privacy Policy
+          </a>
+          <a className={footerStyles.footerLink} href="#">
+            Terms and Conditions
+          </a>
         </div>
       </div>
     </footer>
