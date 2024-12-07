85e82c81c69ace007038ffa35ee5fa894e896c4f
Aleksandr Alekhin <alekhin.aleks92@gmail.com>
2024-12-07 12:05:14 +0100

    refactoring

--- src/ui/Modal/Modal.tsx
+++ src/ui/Modal/Modal.tsx
@@ -1,19 +1,21 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import styles from "./index.module.css";
-import {Portal} from "../Portal/Portal";
+import { Portal } from "../Portal/Portal";
 
 export type ModalProps = {
-    children: React.ReactNode;
-    onClose: () => void;
+  children: React.ReactNode;
+  onClose: () => void;
 };
 
-export const Modal: FC<ModalProps> = ({ children, onClose }) => <Portal>
+export const Modal: FC<ModalProps> = ({ children, onClose }) => (
+  <Portal>
     <div className={styles.overlay}>
-        <div className={styles.content}>
-            <button className={styles.closeButton} onClick={onClose}>
-                X
-            </button>
-            {children}
-        </div>
+      <div className={styles.content}>
+        <button className={styles.closeButton} onClick={onClose}>
+          X
+        </button>
+        {children}
+      </div>
     </div>
-</Portal>
+  </Portal>
+);
