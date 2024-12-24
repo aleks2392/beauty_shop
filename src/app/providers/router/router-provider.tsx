b74bcddd979a89ce2bc39a0852f607e6987e273d
Aleksandr Alekhin <alekhin.aleks92@gmail.com>
2024-12-24 11:59:50 +0100

    fix

--- src/app/providers/router/router-provider.tsx
+++ src/app/providers/router/router-provider.tsx
@@ -1,7 +1,10 @@
 import { RouterProvider as ReactRouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { FC } from "react";
+import React, { Suspense } from "react";
 
 export const RouterProvider: FC = () => (
-  <ReactRouterProvider router={router} fallbackElement={<p>Loading...</p>} />
+  <Suspense fallback={<p>Loading...</p>}>
+    <ReactRouterProvider router={router} />
+  </Suspense>
 );
