71b837ab5d24d88ce92ffec549266072870c3089
Danilov Dmitriy <dima-danilov1995@gmail.com>
2024-12-25 22:58:42 +0300

    cart reducer

--- src/app/providers/store/slices/cart/slice.ts
+++ src/app/providers/store/slices/cart/slice.ts
@@ -22,4 +22,4 @@ export const cartSlice = createSlice({
 
 export const { addProduct } = cartSlice.actions;
 
-export const productsReducer = cartSlice.reducer;
+export const cartReducer = cartSlice.reducer;
